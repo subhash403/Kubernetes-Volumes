4a96a5d3a4b7fdcfb52b14215663940f1c5b1521
subhash403 <rathnasubhash3@gmail.com>
2020-05-23 18:21:11 +0530

    added linkerd yamls

---
+++ canary.sh
@@ -0,0 +1,28 @@
+cat <<EOF | kubectl apply -f -
+apiVersion: flagger.app/v1beta1
+kind: Canary
+metadata:
+  name: podinfo
+  namespace: test
+spec:
+  targetRef:
+    apiVersion: apps/v1
+    kind: Deployment
+    name: podinfo
+  service:
+    port: 9898
+  analysis:
+    interval: 50s
+    threshold: 5
+    stepWeight: 10
+    maxWeight: 100
+    metrics:
+    - name: request-success-rate
+      thresholdRange:
+        min: 99
+      interval: 1m
+    - name: request-duration
+      thresholdRange:
+        max: 500
+      interval: 5m
+EOF
